be6b427dda9e22520695673636bcb745e45eac7a
Gabriel Caldeira <gabrielcalddeira270@gmail.com>
2023-06-12 00:29:20 -0300

    carinha

--- pedido/js/app.js
+++ pedido/js/app.js
@@ -7,7 +7,7 @@ const $btnYes = document.querySelector('.js-yes'),
 let counter = 0
 
 $btnYes.addEventListener('mouseover', (e) => {
-    e.target.innerHTML = 'UM CLICK'
+    e.target.innerHTML = 'UM CLICK 🥺'
 })
 
 $btnYes.addEventListener('click', () => {
